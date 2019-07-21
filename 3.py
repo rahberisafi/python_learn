4b4ad354835c2b7a4975a292a90a052c5d1b931b
iamisafi <isafi@gmail.com>
2019-07-21 14:33:01 +0600

    function in python

--- 3.py
+++ 3.py
@@ -1,6 +1,12 @@
-grade = 99
+number = 70
 
-if grade == 100:
-    print("You Passed")
+if (number < 100 or number >= 300) and (number % 3 == 0 or number % 7 == 0):
+    if (number % 3 == 0 and number % 7 == 0):
+        print("Divisible by both")
+    elif (number % 3 == 0):
+        print("Divisible by 3")
+    else:
+        print("Divisible by 7")
+
 else:
-    print("You Failed")
+        print("Not a special number")
