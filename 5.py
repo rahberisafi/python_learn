aefd0ac55bc6fe97aeb8fe46807666a2a08a2c27
iamisafi <isafi@gmail.com>
2019-07-23 14:19:56 +0600

    add pythob list

---
+++ 5.py
@@ -0,0 +1,25 @@
+#list & tuples
+
+grocery_list = ["onion", "apple", "sugar", "noodles", "eggs", "orange"]
+
+
+# grocery_list.append("rice")
+
+# grocery_list.pop(1)
+
+# position = [100, 300]
+#
+# print(position[1])
+
+# print(grocery_list[0:4])
+
+
+# grocery_list.insert(2, "soap") // insert
+
+# print(grocery_list.index("sugar"))
+
+grocery_list.remove("sugar")
+
+# print(len(grocery_list))
+
+print(grocery_list)
