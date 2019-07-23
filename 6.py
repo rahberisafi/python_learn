4da1d1b38e8d2d79526ee304ffda88f912c0101a
iamisafi <isafi@gmail.com>
2019-07-23 14:34:39 +0600

    python sort

---
+++ 6.py
@@ -0,0 +1,24 @@
+# grades = [100, 52, 66, 14, 2, 33, 17, 79]
+grades = [100, 52, 66, 14, 2, 33, 17, 79, 100, 100]
+
+
+print(grades.count(100))
+
+grades.pop()
+grades.pop()
+
+print(grades.count(100))
+
+
+
+# sort
+
+# grades.sort()
+#
+# print(grades)
+
+# reverse order
+
+# grades.reverse()
+#
+# print(grades)
