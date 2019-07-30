a539ec609f7dba6ed3593b90d5be3841100e43ee
iamisafi <isafi@gmail.com>
2019-07-30 14:24:31 +0600

    add for loop

---
+++ 10.py
@@ -0,0 +1,36 @@
+# grocery_list = ["milk", "toast", "eggs", "avocados"]
+#
+# for item in grocery_list:
+#     print(item)
+
+# weights = [20, 50, 4, 41, 14]
+#
+# total_weight = 0
+#
+# for weight in weights:
+#     total_weight += weight
+#
+# print(total_weight)
+
+# weights = [20, 50, 4, 41, 14]
+#
+# total_weight = 0
+#
+# for index, weight in enumerate(weights):
+#     print(index)
+#     print(weight)
+#
+# print(total_weight)
+#
+# number_of_players = len(weights)
+#
+# print(number_of_players)
+#
+# print(total_weight/number_of_players)
+
+i = 0
+
+while i < 10:
+    print(i)
+    print(i<10)
+    i += 1
